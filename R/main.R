6c738bd20b62777774ede3a75ad79f28d482b7fc
ajfedorec <ajfedorec@gmail.com>
2018-09-04 16:29:43 +0100

    updated to include limits argument

--- R/main.R
+++ R/main.R
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-bayesPairs <- function(posteriors, priors){
+bayesPairs <- function(posteriors, priors, limits){
   n_params <- ncol(posteriors)
 
   plt.list <- list()
@@ -18,7 +18,7 @@ bayesPairs <- function(posteriors, priors){
       j <- j
       plt.idx <- plt.idx
       # print(paste("i", i, "j", j))
-      if((i == 1) & (j == 1)) {
+      if((i == 1) & (j == 1)) { ## TOP CORNER BLANK
         plt <- ggplot2::ggplot() +
           ggplot2::geom_point(ggplot2::aes(1,1), colour = 'white')+
           ggplot2::theme(plot.background = ggplot2::element_blank(),
@@ -30,29 +30,29 @@ bayesPairs <- function(posteriors, priors){
                          axis.title.y = ggplot2::element_blank(),
                          axis.text.x = ggplot2::element_blank(),
                          axis.text.y = ggplot2::element_blank(),
-                         axis.ticks = ggplot2::element_blank())
+                         axis.ticks = ggplot2::element_blank(),
+                         axis.line = ggplot2::element_blank())
       }
-      else if((i == 1)){
-        ## plot top row rug plot
+      else if((i == 1)){ ## TOP RUG PLOTS
         plt <- ggplot2::ggplot() +
           # ggplot2::geom_density(data=posteriors, ggplot2::aes(posteriors[j-1]), size=0.2)+
           ggplot2::stat_density(data=posteriors, ggplot2::aes(posteriors[j-1]),
                                 size=0.2, geom = "line", position = "identity")+
           # ggplot2::geom_density(data=priors, ggplot2::aes(priors[,j-1]), colour="red")+
-          ggplot2::scale_x_continuous(limits = c(min(priors[,j-1]), max(priors[,j-1])))+
+          # ggplot2::scale_x_continuous(limits = c(min(priors[,j-1]), max(priors[,j-1])))+
+          ggplot2::scale_x_continuous(limits = c(limits[j-1,1], limits[j-1,2]))+
           theme_ajf()+
           ggplot2::theme(axis.title = ggplot2::element_blank(),
                          axis.text.y = ggplot2::element_blank(),
                          axis.ticks.y = ggplot2::element_blank())
       }
-      else if((j == 1)){
-        ## plot RHS rug plot
+      else if((j == 1)){ ## LHS RUG PLOTS
         plt <- ggplot2::ggplot() +
           # ggplot2::geom_density(data=posteriors, ggplot2::aes(posteriors[i-1]), size=0.2)+
           ggplot2::stat_density(data=posteriors, ggplot2::aes(posteriors[i-1]),
                                 size=0.2, geom = "line", position = "identity")+
           # ggplot2::geom_density(data=priors, ggplot2::aes(priors[,i-1]), colour="red")+
-          ggplot2::scale_x_continuous(limits = c(min(priors[,i-1]), max(priors[,i-1]))) +
+          ggplot2::scale_x_continuous(limits = c(limits[i-1,1], limits[i-1,2])) +
           theme_ajf()+
           ggplot2::theme(axis.title = ggplot2::element_blank(),
                          axis.text.x = ggplot2::element_blank(),
@@ -60,7 +60,7 @@ bayesPairs <- function(posteriors, priors){
           ggplot2::coord_flip()+
           ggplot2::scale_y_reverse()
       }
-      else if(i == j){
+      else if(i == j){ ## DIAGONAL PARAM NAMES
         ## plot param name
         plt <- ggplot2::ggplot()+
           ggplot2::annotate("text", x = 4, y = 25, size=4, label = as.character(colnames(posteriors)[i-1]))+
@@ -71,17 +71,17 @@ bayesPairs <- function(posteriors, priors){
                          panel.background = ggplot2::element_blank(),
                          axis.title = ggplot2::element_blank(),
                          axis.text = ggplot2::element_blank(),
-                         axis.ticks = ggplot2::element_blank())
+                         axis.ticks = ggplot2::element_blank(),
+                         axis.line = ggplot2::element_blank())
         # print("diag")
       }
-      else{
-        ## plot 2D density
+      else if(j>i){ ## TOP DIAG DENSITY PLOTS
         plt <- ggplot2::ggplot() +
-          ggplot2::stat_density2d(ggplot2::aes(x = posteriors[j-1], y = posteriors[i-1],
+          ggplot2::stat_density_2d(ggplot2::aes(x = posteriors[j-1], y = posteriors[i-1],
                                                fill=..level..),
                                   geom='polygon', size=0.2)+
-          ggplot2::scale_x_continuous(limits = c(min(priors[,j-1]), max(priors[,j-1])))+
-          ggplot2::scale_y_continuous(limits = c(min(priors[,i-1]), max(priors[,i-1])))+
+          ggplot2::scale_x_continuous(limits = c(limits[j-1,1], limits[j-1,2]))+
+          ggplot2::scale_y_continuous(limits = c(limits[i-1,1], limits[i-1,2]))+
           ggplot2::scale_fill_continuous(guide="none", low="yellow",high="red")+
           ggplot2::scale_alpha_continuous(guide="none")+
           theme_ajf()+
@@ -89,6 +89,19 @@ bayesPairs <- function(posteriors, priors){
                          axis.text = ggplot2::element_blank(),
                          axis.ticks = ggplot2::element_blank())
         # print("2D density")
+      }
+      else { ## BOTTOM DIAG DENSITY PLOTS
+        plt <- ggplot2::ggplot() +
+          ggplot2::geom_density_2d(ggplot2::aes(x = posteriors[j-1], y = posteriors[i-1],
+                                               colour=..level..), size=0.2)+
+          ggplot2::scale_x_continuous(limits = c(limits[j-1,1], limits[j-1,2]))+
+          ggplot2::scale_y_continuous(limits = c(limits[i-1,1], limits[i-1,2]))+
+          ggplot2::scale_colour_continuous(guide="none", low="yellow",high="red")+
+          ggplot2::scale_alpha_continuous(guide="none")+
+          theme_ajf()+
+          ggplot2::theme(axis.title = ggplot2::element_blank(),
+                         axis.text = ggplot2::element_blank(),
+                         axis.ticks = ggplot2::element_blank())
       }
 
       # show(plt)
